c7a27d9de3dea5b31f8acbe05502f4101dc46d5a
vaidyapusainikhil <89408610+vaidyapusainikhil@users.noreply.github.com>
2021-10-26 23:15:17 -0500

    editing users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var count = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  count = count + 1;
+  res.send(`User accesses are ${count}`);
 });
 
 module.exports = router;
